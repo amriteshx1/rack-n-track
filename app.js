f21fec601374ccf0324ba60fd9682ef3961254e3
amriteshx1 <amriteshx18@gmail.com>
2025-04-13 16:44:01 +0530

    set up Express app with EJS view engine, static assets, and base router

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//this is entry
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const userRouter = require('./routes/userRouter');
+const port = process.env.PORT || 3000;
+
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+// Routes
+app.use('/', userRouter);
+
+// Start server
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
+});
