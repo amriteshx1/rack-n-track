1f9f8987e1ed420cbf41c2c970f3a06c90e81907
amriteshx1 <amriteshx18@gmail.com>
2025-04-13 17:07:49 +0530

    add controller handlers to render homepage, category, and item views

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,11 @@
-//hi this is usercontroller
+exports.homePageHandle = (req, res) => {
+    res.render('index', { title: 'Homepage' });
+}
+
+exports.categoryPageHandle = (req, res) => {
+    res.render('category', { title: 'Category Page' });
+}
+
+exports.itemPageHandle = (req, res) => {
+    res.render('item', { title: 'Item Page' });
+}
