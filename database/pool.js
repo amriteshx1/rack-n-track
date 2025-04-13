a0e630b793b4aafcb07b6b6be96dda7c84db06b9
amriteshx1 <amriteshx18@gmail.com>
2025-04-13 19:28:05 +0530

    add PostgreSQL connection setup using dotenv and Pool

--- database/pool.js
+++ database/pool.js
@@ -1,1 +1,9 @@
-//hi this a pool connection
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+module.exports = pool;
